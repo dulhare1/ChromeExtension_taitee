14c6d55f51e157238500deba85ca7d1de7bfea27
Ray <dulhare1+github@gmail.com>
2018-06-10 23:30:37 +0800

    puyuma only

--- content.js
+++ content.js
@@ -6,8 +6,8 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     sendResponse({ res: "來自內容腳本的回覆" });
 
     //取得popup html儲存的訊息
-    chrome.storage.sync.get(['person_id', 'getin_date', 'from_station', 'to_station', 'train_no', 'order_qty_str',], function (data) {
-
+    chrome.storage.sync.get(['person_id', 'getin_date', 'from_station', 'to_station', 'train_no', 'n_order_qty_str','z_order_qty_str'], function (data) {
+        console.log(data);
         addInput(data);
 
         var searchButton = document.getElementsByClassName('btn-primary');
@@ -28,14 +28,16 @@ var addInput = function (data) {
     var from_station = document.getElementById('from_station');
     var to_station = document.getElementById('to_station');
     var train_no = document.getElementById('train_no');
-    var order_qty_str = document.getElementById('order_qty_str');
+    var n_order_qty_str = document.getElementById('n_order_qty_str');
+    var z_order_qty_str = document.getElementById('z_order_qty_str');
 
     person_id.value = data.person_id;
     getin_date.value = data.getin_date;
     from_station.value = data.from_station;
     to_station.value = data.to_station;
     train_no.value = data.train_no;
-    order_qty_str.value = data.order_qty_str;
+    n_order_qty_str.value = data.n_order_qty_str;
+    z_order_qty_str.value = data.z_order_qty_str;
 }
 
 
--- popup.js
+++ popup.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', function (dcle) {
     var from_station = document.getElementById('from_station');
     var to_station = document.getElementById('to_station');
     var train_no = document.getElementById('train_no');
-    var order_qty_str = document.getElementById('order_qty_str');
+    var n_order_qty_str = document.getElementById('n_order_qty_str');
+    var z_order_qty_str = document.getElementById('z_order_qty_str');
     var randInputErrorBack = document.getElementsByName('randInputErrorBack');
 
     var today = new Date();
@@ -21,10 +22,30 @@ document.addEventListener('DOMContentLoaded', function (dcle) {
         today.setDate(today.getDate() + 1);
         beforehandDay -= 1;
     }
-    //產生日期選單 
-    for (var i = 0; i < beforehandDay; i++) {
-        let formatDay = today.getFullYear() + "/" + paddingLeft((today.getMonth() + 1).toString(), 2) + "/" + paddingLeft(today.getDate().toString(), 2);
-        let date_value = formatDay + '-' + paddingLeft(i.toString(), 2);
+    //產生日期選單 有bug，台鐵儲存日期會變動但這邊的儲存日期不會變動!!!
+    // for (var i = 0; i < beforehandDay; i++) {
+    //     let formatDay = today.getFullYear() + "/" + paddingLeft((today.getMonth() + 1).toString(), 2) + "/" + paddingLeft(today.getDate().toString(), 2);
+    //     let date_value = formatDay + '-' + paddingLeft(i.toString(), 2);
+    //     let theOption = document.createElement("OPTION");
+    //     theOption.setAttribute("value", date_value);
+    //     let text = document.createTextNode(formatDay);
+    //     theOption.appendChild(text);
+    //     getin_date.appendChild(theOption);
+
+    //     today.setDate(today.getDate() + 1);
+    // }
+    let formatDay1 = '2018/06/13';
+        let date_value1 = '2018/06/13-02';
+        let theOption1 = document.createElement("OPTION");
+        theOption1.setAttribute("value", date_value1);
+        let text1 = document.createTextNode(formatDay1);
+        theOption1.appendChild(text1);
+        getin_date.appendChild(theOption1);
+
+        today.setDate(today.getDate() + 1);
+
+        let formatDay = '2018/06/25';
+        let date_value = '2018/06/25-14';
         let theOption = document.createElement("OPTION");
         theOption.setAttribute("value", date_value);
         let text = document.createTextNode(formatDay);
@@ -32,11 +53,10 @@ document.addEventListener('DOMContentLoaded', function (dcle) {
         getin_date.appendChild(theOption);
 
         today.setDate(today.getDate() + 1);
-    }
 
 
     //放入儲存值
-    chrome.storage.sync.get(['person_id', 'getin_date', 'from_station', 'to_station', 'train_no', 'order_qty_str', 'errorBack'], function (data) {
+    chrome.storage.sync.get(['person_id', 'getin_date', 'from_station', 'to_station', 'train_no', 'n_order_qty_str','z_order_qty_str', 'errorBack'], function (data) {
         if (data.person_id) {
             console.log('有東西!');
             console.log(data);
@@ -45,7 +65,8 @@ document.addEventListener('DOMContentLoaded', function (dcle) {
             from_station.value = data.from_station;
             to_station.value = data.to_station;
             train_no.value = data.train_no;
-            order_qty_str.value = data.order_qty_str;
+            n_order_qty_str.value = data.n_order_qty_str;
+            z_order_qty_str.value=data.z_order_qty_str;
             data.errorBack ? randInputErrorBack[0].checked = true : randInputErrorBack[1].checked = true;
         } else {
             console.log('空的!');
@@ -66,7 +87,8 @@ document.addEventListener('DOMContentLoaded', function (dcle) {
             'from_station': from_station.value,
             'to_station': to_station.value,
             'train_no': train_no.value,
-            'order_qty_str': order_qty_str.value,
+            'n_order_qty_str': n_order_qty_str.value,
+            'z_order_qty_str':z_order_qty_str.value,
             'errorBack': errorBack
         }, function () {
             console.log('person id is ' + person_id.value);
